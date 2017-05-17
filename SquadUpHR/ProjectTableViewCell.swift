1c42747714af9d6aa919db2191b3275d1a70ae40
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-05-17 12:23:10 +0800

    profile added

---
+++ SquadUpHR/ProjectTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProjectTableViewCell.swift
+//  SquadUpHR
+//
+//  Created by nicholaslee on 16/05/2017.
+//  Copyright © 2017 Max Jala. All rights reserved.
+//
+
+import UIKit
+
+class ProjectTableViewCell: UITableViewCell {
+
+    static let cellIdentifier = "ProjectTableViewCell"
+    static let cellNib = UINib(nibName: ProjectTableViewCell.cellIdentifier, bundle: Bundle.main)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
